48d6b2808b181313180049c502776e76c00087a6
CanonicCake <n.a.ellis99@gmail.com>
2023-01-27 13:17:45 -0800

    c in print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry int
+ *
+ * Return: 0 Always (Success)
+ */
+
+int main(void)
+
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
