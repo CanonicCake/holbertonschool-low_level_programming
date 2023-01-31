a6759499d570f2069673c9e8d6e55a0a15d2fa44
CanonicCake <n.a.ellis99@gmail.com>
2023-01-31 09:40:53 -0800

    printing alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -14,12 +14,12 @@ int main(void)
 	char ch = 'a';
 	char CH = 'A';
 
-	while (ch != 'z')
+	while (ch <= 'z')
 	{
 		putchar(ch);
 		ch++;
 	}
-	while (CH != 'Z')
+	while (CH <= 'Z')
 	{
 		putchar(CH);
 		CH++;
