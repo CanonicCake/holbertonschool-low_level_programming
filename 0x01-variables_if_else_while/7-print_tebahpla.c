cde167502f29985a5e598085055d978faa101175
CanonicCake <n.a.ellis99@gmail.com>
2023-02-02 08:44:00 -0800

    backwards

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+
+	int i = 122;
+
+	for (; i >= 97; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}
