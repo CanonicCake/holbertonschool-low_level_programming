078b0de416f3a28b3baa4a8f2c83c6b9666c0947
CanonicCake <n.a.ellis99@gmail.com>
2023-02-06 13:01:55 -0800

    adds d and n

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - entry to add
+ * @n: variable
+ * @d: variable
+ *
+ * Return: n and d
+ */
+
+int add(int n, int d)
+{
+	int add;
+
+	add = (d + n);
+	return (add);
+}
