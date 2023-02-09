f342ec79c4d04088ba5ae549707e84ae8d047990
CanonicCake <n.a.ellis99@gmail.com>
2023-02-09 11:57:06 -0800

    will multiply a and b

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - will multiply
+ * @a: variable 1
+ * @b: variable 2
+ *
+ * Return: mul
+ */
+
+int mul(int a, int b)
+{
+	int multi;
+
+	multi = (a * b);
+		return (multi);
+	_putchar('\n');
+}
