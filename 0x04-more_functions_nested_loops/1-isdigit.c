4fdcaa517cf7260feb5f6cff4348c50d758de08a
CanonicCake <n.a.ellis99@gmail.com>
2023-02-09 11:35:19 -0800

    will tell if its digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isdigit - will tell if c is a digit
+ * @c: variable
+ *
+ * Return: 1 if a digit, 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
