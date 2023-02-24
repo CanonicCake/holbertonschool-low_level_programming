7eac766df412a6b5834e1fd7dde62b4bdf68689a
CanonicCake <n.a.ellis99@gmail.com>
2023-02-24 09:00:16 -0800

    arguments and responces

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point to check
+ * @argc: argument count
+ * @argv: argument value
+ *
+ * Return: 0 Always (success)
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
