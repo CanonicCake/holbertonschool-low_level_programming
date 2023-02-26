36c982f15ed14c44613254ea03818c8f83dad11b
CanonicCake <n.a.ellis99@gmail.com>
2023-02-26 10:38:56 -0800

    multiply two numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - entry to check code
+ * @argc: argument count
+ * @argv: argument value
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+
+	if (argc == 1)
+	
+	{
+		printf("ERROR\n"
+		return (1);
+	}
+}
