1f3699f5759aed12297257a27848708b66689ef9
CanonicCake <n.a.ellis99@gmail.com>
2023-05-26 11:43:28 -0700

    changing to define

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -4,16 +4,22 @@
 /**
  * main - Entry point
  *
- * Return: Always 0
+ * Return: Always 0 (success)
  */
 
 int main(void)
 
 {
-	printf("Size of char: %lu byte(s)\n", (int)sizeof(char));
-	printf("Size of an int: %lu byte(s)\n", (int)sizeof(int));
-	printf("Size of a long int: %lu byte(s)\n", (int)sizeof(long int));
-	printf("Size of a long long int: %lu byte(s)\n", (int)sizeof(long long int));
-	printf("Size of a float: %lu byte(s)\n", (int)sizeof(float));
+	char c;
+	int i;
+	long int l;
+	long long int e;
+	float f;
+
+	printf("Size of char: %lu byte(s)\n", (int)sizeof(c));
+	printf("Size of an int: %lu byte(s)\n", (int)sizeof(i));
+	printf("Size of a long int: %lu byte(s)\n", (int)sizeof(l));
+	printf("Size of a long long int: %lu byte(s)\n", (int)sizeof(e));
+	printf("Size of a float: %lu byte(s)\n", (int)sizeof(f));
 	return (0);
 }
