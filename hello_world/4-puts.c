bbd73ef88f768c86aaa5040c7a8e993c38295f23
CanonicCake <n.a.ellis99@gmail.com>
2023-05-26 11:20:14 -0700

    directory change

---
+++ hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 Always (Sucess)
+ */
+
+int main(void)
+
+{
+	puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
