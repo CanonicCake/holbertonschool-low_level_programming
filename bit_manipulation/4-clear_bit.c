6173c5f979568313bd708a9c13293d318b2000d7
CanonicCake <n.a.ellis99@gmail.com>
2023-07-17 16:04:38 -0700

    quick fix

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -9,7 +9,7 @@
  * Return: 1 on success, -1 on fail
  */
 
-int clear_bit(unsigned long int n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if(index > sizeof(long) * 8)
 	{
