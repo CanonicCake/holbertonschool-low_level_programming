5b639d1581e2bcf973f5b08a5cd09f50560a4d7c
CanonicCake <n.a.ellis99@gmail.com>
2023-08-01 00:37:02 -0700

    v2

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -16,7 +16,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new = NULL, *node = NULL;
 
 	if (ht == NULL)
-		retrun (0);
+		return (0);
 
 	idx = key_index((const unsigned char *)key, ht->size);
 
