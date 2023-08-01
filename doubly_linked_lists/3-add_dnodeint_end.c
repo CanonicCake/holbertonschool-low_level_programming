43b29b72508df6e37bdc06bbef8fc5024d76546e
CanonicCake <n.a.ellis99@gmail.com>
2023-07-31 20:35:30 -0700

    adding to the end

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -21,7 +21,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	if (*head)
 	{
 		current = *head;
-		while (!current->next)
+		while (current->next != NULL)
 		{
 			current = current->next;
 		}
