427ad36e9a98384bab4a62aaabe45a4bc8bef182
CanonicCake <n.a.ellis99@gmail.com>
2023-07-31 20:21:51 -0700

    add a new head, relink everything

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - adds a node at the beginning
+ * @head: head of the linkned list
+ * @h: variable
+ *
+ * Return: the new head
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new = NULL;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+
+	new->n = n;
+	if(*head)
+	{
+		new->next = *head;
+		new->prev = (*head)->prev;
+		(*head)->prev = new;
+		*head = new;
+		return (*head);
+	}
+
+	new->next = *head;
+	new->prev = NULL;
+	*head = new;
+	return (*head);
+}
