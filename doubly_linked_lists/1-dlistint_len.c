e68c5b5c7cde24fed4d4321995f0bfa7ebfd3adf
CanonicCake <n.a.ellis99@gmail.com>
2023-07-31 20:11:34 -0700

    prints the length

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - prints the length of a list
+ * @h: variable of nodes
+ *
+ * Return: Length of list
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	int len = 0;
+
+	while (h != NULL)
+	{
+		++len;
+		h = h-> next;
+	}
+	return (len);
+}
