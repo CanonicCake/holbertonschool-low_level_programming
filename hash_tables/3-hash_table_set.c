1ce6d66997d8da056592564d881234ef69516185
CanonicCake <n.a.ellis99@gmail.com>
2023-08-01 00:58:24 -0700

    v6

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -13,39 +13,30 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *head, *node;
+	hash_node_t *head = NULL, *node = NULL;
 
 	if (ht == NULL)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
+	head = ht->array[index];
+	
+	if (head && strcmp(key, head->key) == 0)
+	{
+		free(head->value);
+		head->value = strdup(value);
+		return (1);
+	}
 
 	node = malloc(sizeof(hash_node_t));
-	if (node == NULL)
+	if (new_node == NULL)
 	{
 		return (0);
 	}
 
 	node->key = strdup(key);
 	node->value = strdup(value);
-	node>next = NULL;
-
-	if (ht->array[index] == NULL)
-	{
-		ht->array[index] = node;
-	}
-	else
-	{
-		head = ht->array[index];
-		if (strdup(key, head->key) == 0)
-		{
-			head->value = strdup(value);
-		}
-		else
-		{
-			node->next = head;
-			ht->array[index] = node;
-		}
-	}
+	node->next = ht->array[index];
+	ht->array[index] = node;
 	return (1);
 }
