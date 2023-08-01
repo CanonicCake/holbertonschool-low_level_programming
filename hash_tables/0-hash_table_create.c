256e9331838b17c26a2a8f8fe13a749da32c0a9c
CanonicCake <n.a.ellis99@gmail.com>
2023-07-31 23:08:56 -0700

    betty and forgot *

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -15,15 +15,18 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
+	{
 		return (NULL);
-
-	array = malloc(sizeof(hash_node_t) * size);
+	}
+	array = malloc(sizeof(hash_node_t *) * size);
 	if (array == NULL)
+	{
 		return (NULL);
-
+	}
 	for (; i < size; ++i)
+	{
 		array[i] = NULL;
-
+	}
 	table->size = size;
 	table->array = array;
 
