fb5e87866c74f34dca3a52f4967b20d7a15b3f4a
CanonicCake <n.a.ellis99@gmail.com>
2023-07-31 22:00:23 -0700

    deleting

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,75 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - find len
+ * @h: head
+ *
+ * Return: nodes
+ */
+
+size_t dlistint_t(const dlistint_t *h)
+{
+	int counter = 0;
+
+	while (h)
+	{
+		counter++;
+		h = h->next;
+	}
+	return (counter);
+}
+
+/**
+ * delete_dnodeint_at_index - deletes at index
+ * @head: the head of the list
+ * index: index to delete at
+ *
+ * Return: 1 on success, -1 if failed
+ */
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *current;
+	size_t len;
+	unsigned int i = 0;
+
+	if (head == NULL || *head == NULL)
+		return (-1);
+
+	len = dlistint_len(*head);
+	if (index >= len)
+		return (-1);
+
+	current = *head;
+	if (index == 0)
+	{
+		*head = current->next;
+		if (*head != NULL)
+		{
+			(*head)->prev = NULL;
+		}
+		free(current);
+		return (1);
+	}
+	if (index == (len - 1))
+	{
+		while (curent->next)
+		{
+			current = current->next;
+		}
+		free(current);
+	}
+	while (current)
+	{
+		if (i == index)
+		{
+			current->next->prev = current->prev;
+			current->prev->next = curent->next;
+			free(current);
+			return (1);
+		}
+		current = current->next;
+		i++;
+	}
+	return (-1);
+}
