63ee6e0c06dcbaa1a4118a45b37825b770615f99
CanonicCake <n.a.ellis99@gmail.com>
2023-07-31 20:49:15 -0700

    fixed prototype

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -8,7 +8,7 @@
  * Return: node
  */
 
-dlistint_t *get_dnode_at_index(dlistint_t *head, unsigned int index)
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	dlistint_t *current = head;
 	unsigned int counter = 0;
