0f6aaf75ed879ed92e4bda70d7c9abbc771fb433
CanonicCake <n.a.ellis99@gmail.com>
2023-08-01 00:59:41 -0700

    v7

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -29,7 +29,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 
 	node = malloc(sizeof(hash_node_t));
-	if (new_node == NULL)
+	if (node == NULL)
 	{
 		return (0);
 	}
