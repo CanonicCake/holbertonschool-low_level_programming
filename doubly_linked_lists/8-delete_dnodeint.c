db63b93a96f7bac3730f9c31360995700137037b
CanonicCake <n.a.ellis99@gmail.com>
2023-07-31 22:35:54 -0700

    v6

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -97,7 +97,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 				return (current);
 			}
 			
-			currnet = current->next;
+			current = current->next;
 			++counter;
 		}
 	}
