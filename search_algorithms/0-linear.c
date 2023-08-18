ce922f365d87d743b498abd0c953a0e7370f264b
CanonicCake <n.a.ellis99@gmail.com>
2023-08-18 13:45:39 -0700

    starting linear search

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,25 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - search for an array of integers
+ * @array: pointer to the first element of the array to search
+ * @size:number of elements in an array
+ * @value: value
+ *
+ * Return: value
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i, j;
+
+	if (array == NULL || array != value)
+	{
+		return (-1);
+	}
+	for (i = 0
+	{
+		printf("Value checked array[%d] = [%d]", size, value);
+	}
+	return (value);
+}
