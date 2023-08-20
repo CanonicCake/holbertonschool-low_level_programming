4b2c7b354695f4bccefaf2a36ef031368a8b85af
CanonicCake <n.a.ellis99@gmail.com>
2023-08-19 20:11:44 -0700

    print hash table

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table
+ * @ht: hash table
+ *
+ * Return: void
+ */
+
+void hash_table_print(count hash_table_t *ht)
+{
+	size_t i = 0;
+	hash_node_t temp;
+	int check;
+
+	if (ht == NULL)
+		return;
+	printf("{");
+	while (i < ht->size)
+	{
+		temp = ht->array[i];
+		while (temp != NULL)
+		{
+			if (check == 1)
+				printf(", ");
+			printf("'%s': '%s'", temp->key, temp-value);
+			temp = temp->next;
+			check = 1;
+		}
+		i++;
+	}
+	printf("}\n");
+}
